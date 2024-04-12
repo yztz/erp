2eda1007f937fb35d1bf0a58b40b2cc2885be673
yztz <781089956@qq.com>
2024-04-13 01:53:37 +0800

    add deploy.sh & fix '-r' for mv

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tmux kill-session -t wms
+git fetch --all
+git reset --hard origin/master
+# 前端
+cd frontend
+yarn 
+yarn build 
+yarn deploy-linux
+# 后端
+cd ../backend
+yarn
+yarn build
+tmux new -s session -d "NODE_ENV=production yarn start; bash"
