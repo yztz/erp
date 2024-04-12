1ce703685fef051731743237fc19751d732a37b6
yztz <781089956@qq.com>
2024-04-13 02:24:53 +0800

    fix permission again and again and again

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+
+# 设置重试间隔时间
+retry_interval=3  # seconds
+
 source /home/ubuntu/.bashrc
 tmux kill-session -t wms
-git fetch --all
+
+# 无限循环
+while true; do
+    echo "拉取仓库..."
+    git fetch --all
+
+    # 检查是否成功
+    if [ $? -eq 0 ]; then
+        echo "拉取成功"
+        break
+    else
+        echo "拉取失败，重试..."
+        sleep $retry_interval
+    fi
+done
+
 git reset --hard origin/master
+
 # 前端
 cd frontend
 yarn 
